ee54cbbed2f2d952573dbba8f66ac04fb7b5c60c
khkeipe <kkeipe@gmail.com>
2020-07-09 12:42:13 -0500

    started laying out navbar

--- src/Components/NavBarComponent/NavBarComponent.tsx
+++ src/Components/NavBarComponent/NavBarComponent.tsx
@@ -4,7 +4,26 @@ const NavBarComponent = () => {
 
 	return(
 		<>
-
+		<div className="ui grid">
+			<div className="row black segment">
+				<div className="four wide column center aligned">
+					<h2>Pool</h2>
+				</div>
+				<div className="eight wide column center aligned">
+					
+				</div>
+				<div className="four wide column">
+					
+				</div>
+			</div>
+			<div className="three wide column black">
+				<div className="ui vertical menu fluid">
+					<div className="item">
+						<div className="header">Test Header</div>
+					</div>
+				</div>
+			</div>
+		</div>
 		</>
 	);
 }
