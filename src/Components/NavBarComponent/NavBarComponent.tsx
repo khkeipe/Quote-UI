0d681d739e90a24908b12bbcfe50ce3d081914da
khkeipe <kkeipe@gmail.com>
2020-07-10 14:44:35 -0500

    convert to class based

--- src/Components/NavBarComponent/NavBarComponent.tsx
+++ src/Components/NavBarComponent/NavBarComponent.tsx
@@ -1,31 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Menu } from 'semantic-ui-react';
 
-const NavBarComponent = () => {
+class NavBarComponent extends Component{
 
-	return(
-		<>
-		<div className="ui grid">
-			<div className="row black segment">
-				<div className="four wide column center aligned">
-					<h2>Pool</h2>
-				</div>
-				<div className="eight wide column center aligned">
-					
-				</div>
-				<div className="four wide column">
-					
-				</div>
-			</div>
-			<div className="three wide column black">
-				<div className="ui vertical menu fluid">
-					<div className="item">
-						<div className="header">Test Header</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		</>
-	);
+	render(){
+		return(
+			<>
+				<Menu vertical>
+
+				</Menu>
+			
+			</>
+		);
+	}
 }
 
 export default NavBarComponent;
