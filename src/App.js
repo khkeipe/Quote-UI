d2eb4be706ab8c852adb0693b5e9dd9c4e6af148
khkeipe <kkeipe@gmail.com>
2020-07-10 15:26:41 -0500

    remove unused css import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import './App.css';
 import NavBarComponent from './Components/NavBarComponent/NavBarComponent';
 import LoginComponent from './Components/LoginComponent/LoginComponent';
 
