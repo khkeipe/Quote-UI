7a5380f0b4e50d9f763b7ef63017975233d347ef
khkeipe <kkeipe@gmail.com>
2020-07-10 15:27:28 -0500

    starting menu for navbar

--- src/Components/NavBarComponent/NavBarComponent.jsx
+++ src/Components/NavBarComponent/NavBarComponent.jsx
@@ -3,12 +3,23 @@ import { Menu } from 'semantic-ui-react';
 
 class NavBarComponent extends Component{
 	state = {};
-	handleItemClick = (e, { name }) => this.setState({activeItem: name}); 
+	handleItemClick = (e, { name }) => this.setState({ activeItem: name}); 
 
 	render(){
+		const { activeItem } = this.state
+
 		return(
 			<>
 				<Menu vertical>
+					<Menu.Item>
+						<Menu.Header> Menu </Menu.Header>
+						<Menu.Menu>
+							<Menu.Item 
+							name='menu item 1' 
+							active={activeItem === 'menue item 1'} 
+							onClick={this.handleItemClick} />
+						</Menu.Menu>
+					</Menu.Item>
 
 				</Menu>
 			
