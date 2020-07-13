0698f751a311291b085cd7dc0d24d28ee0531a31
khkeipe <kkeipe@gmail.com>
2020-07-13 13:19:12 -0500

    setup reducers for login component

---
+++ src/reducers/login-reducer.js
@@ -0,0 +1,28 @@
+import { SUCCESSFUL_LOGIN, FAILED_INVALID_REQUEST, FAILED_BAD_REQUEST, FAILED_INTERNAL_SERVER_ERROR } from '../actions/action-types';
+
+const initialState = {
+	authUser: null,
+	errorMessage: ''
+}
+
+export const loginReducer = (state = initialState, action) => {
+
+	switch(action.type) {
+		case SUCCESSFUL_LOGIN:
+			return {
+				...state,
+				authUser: action.payload,
+				erroMessage: ''
+			} 
+		case FAILED_INVALID_REQUEST:
+		case FAILED_BAD_REQUEST:
+		case FAILED_INTERNAL_SERVER_ERROR:
+			return {
+				...state,
+				errorMessage: action.payload
+			}
+
+		default:
+			return state
+	}
+}
