919869d3da92709a804aac6aacd6641ed8a0ad70
khkeipe <kkeipe@gmail.com>
2020-07-13 11:13:07 -0500

    adding quote route

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import NavBarComponent from './Components/NavBarComponent/NavBarComponent';
 import LoginComponent from './Components/LoginComponent/LoginComponent';
+import QuoteComponent from './Components/QuoteComponent/QuoteComponent';
 
 function App() {
   return (
     <div>
     	<Router>
 
-			<NavBarComponent/>
+			{/* <NavBarComponent/> */}
 
 			<Switch>
 
 				<Route path="/login" render={() => <LoginComponent/> } />
+				<Route path="/quote" render={() => <QuoteComponent/> } />
 
 			</Switch>
 		</Router>
