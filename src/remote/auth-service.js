cb610988602d22ed757d9c8544e7efe252e5573a
khkeipe <kkeipe@gmail.com>
2020-07-13 15:13:43 -0500

    rename user-client file

--- src/remote/auth-service.js
+++ src/remote/auth-service.js
@@ -1,6 +1,6 @@
-import { userClient } from './UserClient';
+import { userClient } from './user-client';
 
 export async function authenticate(email, password) {
-	let response = await userClient.post('/auth', (email, password));
+	let response = await userClient.post('/auth', { email, password });
 	return await response.data;
 }
