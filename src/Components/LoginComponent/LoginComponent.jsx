55370675d5a030ae34cd3d666728fa8060bbab0d
khkeipe <kkeipe@gmail.com>
2020-07-13 15:14:03 -0500

    work in progress

--- src/Components/LoginComponent/LoginComponent.jsx
+++ src/Components/LoginComponent/LoginComponent.jsx
@@ -1,12 +1,53 @@
 import React from 'react';
+import { Modal, Button, Form, FormButton} from 'semantic-ui-react';
+import { authenticate } from '../../remote/auth-service';
+
+
 
 const LoginComponent = () => {
+	
+	let email;
+	let password;
+
+	window.onkeypress = (event) => {
+		let key = event.key.toUpperCase();
+		if(key === 'ENTER' && Modal.open === true) {
+			login();	
+		}
+	}
+
+	async function login(email, password){
+
+		console.log(email);
+		console.log(password);
+		
+		try {
+			let response = authenticate(email, password);
+			console.log(response);
+			return response;
+		} catch (e) {
+	
+		}
+	}
 
 	return(
 		<>
-			<div className="ui grid center aligned">
-				<button>Get Users</button>
-			</div>
+			<Modal trigger={<Button>LOGIN</Button>} >
+				<Modal.Header>LOGIN</Modal.Header>
+				<Modal.Content>
+					<Form>
+						<Form.Field>
+							<label>Email</label>
+							<input placeholder='Email' value={email} />
+						</Form.Field>
+						<Form.Field>
+							<label>Password</label>
+							<input placeholder='Password' type='password' value={password}/>
+						</Form.Field>
+						<FormButton type='submit' onClick={login}>LOGIN</FormButton>
+					</Form>
+				</Modal.Content>
+			</Modal>
 		</>
 	);
 }
