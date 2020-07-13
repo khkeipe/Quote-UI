f85118042540634bec5df45f679085559c14e8e8
khkeipe <kkeipe@gmail.com>
2020-07-13 11:57:55 -0500

    adding redux devtool connection

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,15 +1,16 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import monitorReducerEnhancer from './enhancers/monitorReducer';
 import loggerMiddleware from './middleware/logger';
-import rootReducer from './reducers'
+import { rootReducer } from './reducers';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 export default function configureStore(preloadedState) {
 	const middleware = [loggerMiddleware, thunkMiddleware];
 	const middlewareEnhancer = applyMiddleware(...middleware)
 
 	const enhancers = [middlewareEnhancer, monitorReducerEnhancer];
-	const composedEnhancers = compose(...enhancers);
+	const composedEnhancers = composeWithDevTools(...enhancers);
 
 	const store = createStore(rootReducer, preloadedState, composedEnhancers);
 	
