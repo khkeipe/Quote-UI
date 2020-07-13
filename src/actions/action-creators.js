1ade09b5a3935f0ec72b8caacec1ec145331c7b1
khkeipe <kkeipe@gmail.com>
2020-07-13 13:18:48 -0500

    setup action for login component

---
+++ src/actions/action-creators.js
@@ -0,0 +1,33 @@
+import { SUCCESSFUL_LOGIN, FAILED_BAD_REQUEST, FAILED_INVALID_REQUEST, FAILED_INTERNAL_SERVER_ERROR } from "./action-types";
+
+import authenticate from '../remote/auth-service';
+
+export const loginAction = (email, password) => async (dispatch) => {
+ 
+	try {
+	let authUser = authenticate(email, password);
+	dispatch({
+		type: SUCCESSFUL_LOGIN,
+		payload: authUser
+	});
+	} catch (e) {
+
+		let status = e.response?.status;
+		if(status === 400) {
+			dispatch({
+				type: FAILED_BAD_REQUEST,
+				payload: e.response?.data.message
+			});
+		} else if (status === 401) {
+			dispatch({
+				type: FAILED_INVALID_REQUEST,
+				payload: e.response?.data.message
+			});
+		} else {
+			dispatch({
+				type: FAILED_INTERNAL_SERVER_ERROR,
+				payload: e.response?.data.message || 'Error: server could not be reached'
+			});
+		}
+	}
+}
