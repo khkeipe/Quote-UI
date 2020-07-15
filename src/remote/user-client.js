d83041bfbe0f2a092e186b8dbb3ad76f262413f4
khkeipe <kkeipe@gmail.com>
2020-07-15 16:38:45 -0500

    switching to depolyed api url

--- src/remote/user-client.js
+++ src/remote/user-client.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export const userClient = axios.create({
-	baseURL: 'http://localhost:8080',
+	// baseURL: 'http://localhost:8080',
+	baseURL: 'http://khkeipepoolapp-env-1.eba-r3vkk32y.us-east-1.elasticbeanstalk.com',
 	headers: {
 		'Content-Type': 'application/json'
 	}
