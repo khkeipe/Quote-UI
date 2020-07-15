fe6ff9ea4c85da4a0862c27542ec84ec2e497d3e
khkeipe <kkeipe@gmail.com>
2020-07-15 15:16:28 -0500

    remove login component now used in nav bar

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import NavBarComponent from './Components/NavBarComponent/NavBarComponent';
 import LoginComponent from './Components/LoginComponent/LoginComponent';
 import QuoteComponent from './Components/QuoteComponent/QuoteComponent';
+import HomeComponent from './Components/HomeComponent/HomeComponent';
 
 function App() {
   return (
     <div>
     	<Router>
 
-			{/* <NavBarComponent/> */}
+			<NavBarComponent/>
 
 			<Switch>
 
-				<Route path="/login" render={() => <LoginComponent/> } />
+				<Route path="/home" render={() => <HomeComponent /> } />
+				{/* <Route path="/login" render={() => <LoginComponent/> } /> */}
 				<Route path="/quote" render={() => <QuoteComponent/> } />
 
 			</Switch>
