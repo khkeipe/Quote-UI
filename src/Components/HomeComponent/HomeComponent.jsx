acd4e8e5ba89de1f97a903f21ee18f4646f3b673
khkeipe <kkeipe@gmail.com>
2020-07-16 13:02:38 -0500

    adding footer and sample text

--- src/Components/HomeComponent/HomeComponent.jsx
+++ src/Components/HomeComponent/HomeComponent.jsx
@@ -1,23 +1,59 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { Button, Container } from 'semantic-ui-react';
+import { Button, Container, Header, Segment, Grid, GridRow, GridColumn } from 'semantic-ui-react';
+import HeaderSubHeader from 'semantic-ui-react/dist/commonjs/elements/Header/HeaderSubheader';
 
 const mapStateToProps = (state) => {
 	return { authUser: state.loginReducer.authUser,
 			 errorMessage: state.loginReducer.errorMessage }
 };
 
+const headerMain = {
+	'margin-top': '3em',
+	'font-size': '4em',
+	'font-weight': 'bold'
+}
+
+const headerSub = {
+	'font-size': '1.7em',
+	'margin-top': '1.5em'
+}
+
+const footer = {
+	'padding': '5em 0em'
+}
+
 const HomeComponent = (props) => {
 
 	return (
 		<>
-			<br/>
-			<Container>
-			
-			<Link to="/quote"> <Button>Quote</Button> </Link>
-			
-			</Container>		
+			<Segment textAlign="center" size="large" vertical padded="very">
+				<Container>
+					<Header as="h1" style={headerMain}>Super Awesome Pools</Header>
+					<HeaderSubHeader as="h2" style={headerSub}> Get a free quote today! </HeaderSubHeader>
+					<Link to="/quote"> <Button size="huge">Start a Quote</Button> </Link>
+				</Container>
+			</Segment>
+			<Segment textAlign="center" vertical inverted style={footer}>
+				<Container>
+					<Grid inverted stackable divided>
+						<GridRow>
+							<GridColumn width="3">
+								<Header as="h4" inverted>About</Header>
+							</GridColumn>
+							<GridColumn width="3">
+							<Header as="h4" inverted>About</Header>
+							</GridColumn>
+							<GridColumn width="7">
+							<Header as="h4" inverted>Footer Header</Header>
+
+							</GridColumn>
+						</GridRow>
+					</Grid>
+				</Container>
+			</Segment>
+					
 		</>
 	)
 };
