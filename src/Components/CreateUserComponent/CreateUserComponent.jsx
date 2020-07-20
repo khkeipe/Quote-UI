792cfe6027fa8ca97524ebd60fe63ef4a5e1266b
khkeipe <kkeipe@gmail.com>
2020-07-20 09:56:56 -0500

    creating sign up component

---
+++ src/Components/CreateUserComponent/CreateUserComponent.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const CreateUserComponent = () => {
+
+
+	return (
+		<>
+
+		</>
+	)
+}
+
+export default connect()(CreateUserComponent);
