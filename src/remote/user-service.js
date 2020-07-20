f214b2997dc70c5f866db03532ceb92f7acfd214
khkeipe <kkeipe@gmail.com>
2020-07-20 15:09:08 -0500

    adding request for creating user

--- src/remote/user-service.js
+++ src/remote/user-service.js
@@ -1,9 +1,20 @@
-import  { userClient } from './UserClient';
+import  { userClient } from './user-client';
 
-export async function getAllUsers(){
+export async function getAllUsers() {
 
 	let response = await userClient.get('/users');
 	console.log(response.data);
 	
+	return response.data;
+}
+
+export async function createNewUser(email, password, passwordVerification) {
+	let response = await userClient.post('/users', {
+		email: email,
+		password: password,
+		passwordVerification: passwordVerification
+	});
+	console.log(response.data);
+
 	return response.data;
 }
