9cd8c2525abf062a8a86237c3cb23384bb6abfe8
khkeipe <kkeipe@gmail.com>
2020-07-21 13:45:28 -0500

    switching back to deployed api

--- src/remote/user-client.js
+++ src/remote/user-client.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export const userClient = axios.create({
-	baseURL: 'http://localhost:5000',
-	// baseURL: 'http://khkeipepoolapp-env-1.eba-r3vkk32y.us-east-1.elasticbeanstalk.com',
+	// baseURL: 'http://localhost:5000',
+	baseURL: 'http://khkeipepoolapp-env-1.eba-r3vkk32y.us-east-1.elasticbeanstalk.com',
 	headers: {
 		'Content-Type': 'application/json'
 	}
