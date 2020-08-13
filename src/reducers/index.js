8f4f3b49421dbd1764c2617d3889894e9f13792b
khkeipe <kkeipe@gmail.com>
2020-08-13 11:55:43 -0500

    adding new quote reducer to combinedreducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
 import { loginReducer } from './login-reducer';
 import { signUpReducer } from './signup-reducer';
+import { quoteReducer } from './quote-reducer';
 
-export const rootReducer = combineReducers({loginReducer, signUpReducer})
+export const rootReducer = combineReducers({loginReducer, signUpReducer, quoteReducer})
