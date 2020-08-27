8b55eb1bc3c22e0c47dab811b57c2f53ea16895a
khkeipe <kkeipe@gmail.com>
2020-08-27 18:03:11 -0500

    change text to cantact

--- src/Components/FooterComponent/FooterComponent.jsx
+++ src/Components/FooterComponent/FooterComponent.jsx
@@ -20,7 +20,7 @@ const FooterComponent = () => {
 							<Header as="h4" inverted>About</Header>
 						</GridColumn>
 						<GridColumn width="3">
-						<Header as="h4" inverted>About</Header>
+						<Header as="h4" inverted>Contact</Header>
 						</GridColumn>
 						<GridColumn width="7">
 						<Header as="h4" inverted>Footer Header</Header>
