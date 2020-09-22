973dac109d230d9e5a04aa8c6041976793ab0ce0
khkeipe <kkeipe@gmail.com>
2020-09-22 17:04:18 -0500

    adding title and style to dashboard

--- src/Components/Dashboard Component/DashboardComponent.jsx
+++ src/Components/Dashboard Component/DashboardComponent.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { loginReducer } from '../../reducers/login-reducer';
+import { Redirect } from 'react-router-dom';
+import { Divider, Grid, GridRow, Header, Segment } from 'semantic-ui-react';
 
 const mapStateToProps = (state) => {
 	return{
@@ -12,13 +13,26 @@ const mapDispatchToProps = {
 
 }
 
-const DashboardComponent = () => {
+const DashboardComponent = (props) => {
 
 
 	return (
+	<>
+		{ !props.authUser ? <Redirect to="/home"/> : 
 		<>
+			<Segment raised>
+				<Grid padded="vertically">
+					<GridRow centered>
+						<Header size="huge">Admin Dashboard</Header>
+					</GridRow>
+					<Divider/>
+					<GridRow>
 
-		</>
+					</GridRow>
+				</Grid>
+			</Segment>
+		</> }
+	</>
 	)
 }
 
