ec57fbbac63840b2f155081ba8a1603eac703642
khkeipe <kkeipe@gmail.com>
2020-09-22 18:56:51 -0500

    adding dtos

---
+++ src/dtos/Dealer.js
@@ -0,0 +1,12 @@
+export class Dealer {
+
+	constructor(dealerName, dealerPhone, dealerEmail, street, city, state, zip) {
+		this.name = dealerName;
+		this.phone = dealerPhone;
+		this.email = dealerEmail;
+		this.street = street;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+	}
+}
