70d94cec041ea8b5f331b4ba175ec39d90d1afe4
khkeipe <kkeipe@gmail.com>
2020-09-26 12:48:36 -0500

    updating actions and reducers for quote success or fail request

--- src/actions/action-creators.js
+++ src/actions/action-creators.js
@@ -1,7 +1,8 @@
-import { SUCCESSFUL_LOGIN, FAILED_BAD_REQUEST, FAILED_INVALID_REQUEST, FAILED_INTERNAL_SERVER_ERROR, SUCCESSFUL_LOGOUT, SUCCESSFUL_QUOTE_CREATION, SUCCESSFUL_CONTACT_CREATION } from "./action-types";
+import { SUCCESSFUL_LOGIN, FAILED_BAD_REQUEST, FAILED_INVALID_REQUEST, FAILED_INTERNAL_SERVER_ERROR, SUCCESSFUL_LOGOUT, SUCCESSFUL_QUOTE_CREATION, QUOTE_UPDATE } from "./action-types";
 
 import { authenticate } from '../remote/auth-service';
 import { createNewUser } from "../remote/user-service";
+import { saveQuote } from '../remote/quote-service';
 
 export const loginAction = (email, password) => async (dispatch) => {
 	
@@ -70,11 +71,39 @@ export const signUpAction = (email, password, passwordVerification) => async (di
 	}
 }
 
+export const quoteUpdateAction = (quote) => async (dispatch) => {
+	dispatch({
+		type: QUOTE_UPDATE,
+		payload: quote
+	});
+}
+
 export const quoteCreatorAction = (quote) => async (dispatch) => {
 
+	try{
+		await saveQuote(quote);
 	dispatch({
 		type: SUCCESSFUL_QUOTE_CREATION,
-		payload: quote
-	})
+		payload: 'Your quote has been submitted successfully'
+	});
+	} catch (e) {
+		let status = e.response?.status;
+		if(status === 400) {
+			dispatch({
+				type: FAILED_BAD_REQUEST,
+				payload: e.response?.data.message
+			});
+		} else if(status === 401) {
+			dispatch({
+				type: FAILED_INVALID_REQUEST,
+				payload: e.response?.data.message
+			});
+		} else {
+			dispatch({
+				type: FAILED_INTERNAL_SERVER_ERROR,
+				payload: e.response?.data.message || 'Error: server could not be reached'
+			});
+		}
+	}
 }
 
--- src/reducers/quote-reducer.js
+++ src/reducers/quote-reducer.js
@@ -1,17 +1,33 @@
-import { SUCCESSFUL_QUOTE_CREATION } from "../actions/action-types"
+import { SUCCESSFUL_QUOTE_CREATION, FAILED_BAD_REQUEST, FAILED_INTERNAL_SERVER_ERROR, FAILED_INVALID_REQUEST, QUOTE_UPDATE } from "../actions/action-types"
 
 const initialState = {
-		quote: {}
+		quote: {},
+		errorMessage: ''
 }
 
 export const quoteReducer = (state = initialState, action) => {
 	switch(action.type) {
+		case QUOTE_UPDATE: {
+			return {
+				...state,
+				quote: action.payload,
+				errorMessage: ''
+			}
+		}
 		case SUCCESSFUL_QUOTE_CREATION: {
 			return {
 				...state,
-				quote: action.payload
+				quote: action.payload,
+				errorMessage: ''
 			}
 		}
+		case FAILED_INVALID_REQUEST:
+		case FAILED_BAD_REQUEST:
+		case FAILED_INTERNAL_SERVER_ERROR:
+			return {
+				...state,
+				errorMessage: action.payload
+			}
 		default:
 			return {
 				...state
