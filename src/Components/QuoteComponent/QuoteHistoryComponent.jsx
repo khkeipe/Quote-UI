d5c635a4dcd153460cf2948c87a5677347708bfa
khkeipe <kkeipe@gmail.com>
2020-09-28 19:01:26 -0500

    create quote history component

---
+++ src/Components/QuoteComponent/QuoteHistoryComponent.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { useEffect } from 'react';
+import { Segment } from 'semantic-ui-react';
+import { getAllQuotes } from '../../remote/quote-service';
+
+const QuoteHistoryComponent = () => {
+
+	const [quotes, setQuotes] = useState([]);
+
+	useEffect(() => {
+	
+		const fetchQuotes = async () => {
+
+		let quoteArray = [];
+
+		let response = await getAllQuotes();
+			for(let quote of response) {
+				quoteArray.push(
+				<div>
+					<Segment>
+						{quote}
+					</Segment>
+				</div>);
+			}
+		
+			setQuotes(quoteArray);
+
+		}
+		fetchQuotes();
+	},[])
+
+	return(
+		<>
+		
+		{quotes}
+
+		</>
+	)
+};
+
+export default QuoteHistoryComponent;
