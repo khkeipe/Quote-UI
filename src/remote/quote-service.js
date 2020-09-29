08b1f32576dd187e64a98279032c4ad0edde456a
khkeipe <kkeipe@gmail.com>
2020-09-28 19:01:42 -0500

    adding request for get all quotes

--- src/remote/quote-service.js
+++ src/remote/quote-service.js
@@ -2,6 +2,11 @@ import { client } from './user-client';
 
 export async function saveQuote(newQuote) {
 
-	let response = await client.post('/quote', newQuote);
+	let response = await client.post('/quotes', newQuote);
 	return response.data;
 };
+
+export async function getAllQuotes() {
+	let response = await client.get('/quotes');
+	return response.data;
+}
