8544bbcf6beab1118d2330f9137ec8b092d9473e
khkeipe <kkeipe@gmail.com>
2020-09-28 19:03:17 -0500

    universal dash with conditional rendering

--- src/Components/Dashboard Component/DashboardComponent.jsx
+++ src/Components/Dashboard Component/DashboardComponent.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Redirect } from 'react-router-dom';
-import { Divider, Grid, GridRow, Header, Segment } from 'semantic-ui-react';
+import { Link, Redirect } from 'react-router-dom';
+import { Button, Divider, Grid, GridRow, Header, Segment } from 'semantic-ui-react';
+import HeaderSubHeader from 'semantic-ui-react/dist/commonjs/elements/Header/HeaderSubheader';
 
 const mapStateToProps = (state) => {
 	return{
@@ -13,24 +14,38 @@ const mapDispatchToProps = {
 
 }
 
+const headerSub = {
+	'fontSize': '1.7em',
+	'marginTop': '1.5em'
+}
+
 const DashboardComponent = (props) => {
 
+	
 
 	return (
 	<>
 		{ !props.authUser ? <Redirect to="/home"/> : 
 		<>
+		
 			<Segment raised>
 				<Grid padded="vertically">
 					<GridRow centered>
-						<Header size="huge">Admin Dashboard</Header>
+						<HeaderSubHeader as="h2" style={headerSub}> Dashboard </HeaderSubHeader>
 					</GridRow>
 					<Divider/>
+					{ props.authUser?.role == 'Admin' ?
 					<GridRow>
 
 					</GridRow>
+					: <></> }
+					<GridRow centered>
+						<Link to="/form"> <Button color='black' size="huge">New Quote</Button> </Link>
+						<Link to="/history"> <Button color='black' size="huge">Quote Hisotry</Button> </Link>
+					</GridRow>
 				</Grid>
 			</Segment>
+		
 		</> }
 	</>
 	)
