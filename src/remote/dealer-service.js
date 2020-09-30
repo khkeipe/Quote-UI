f88783e920bc742ab557145e2d49a161251db833
khkeipe <kkeipe@gmail.com>
2020-09-30 18:11:02 -0500

    change name of dealer request

--- src/remote/dealer-service.js
+++ src/remote/dealer-service.js
@@ -1,6 +1,6 @@
 import {client} from './user-client'
 
-export async function getDealers() {
+export async function getAllDealers() {
 
 	let response = await client.get('/dealers');
 	return response.data;
