e6a1a841a83f6e7a4a73b3a3e83476c413907ad8
khkeipe <kkeipe@gmail.com>
2020-10-02 13:32:58 -0500

    add get dealer by name request

--- src/remote/dealer-service.js
+++ src/remote/dealer-service.js
@@ -5,3 +5,8 @@ export async function getAllDealers() {
 	let response = await client.get('/dealers');
 	return response.data;
 }
+
+export async function getDealerByName(dealerName) {
+	let response = await client.get(`/dealers`, dealerName);
+	return response.data;
+}
