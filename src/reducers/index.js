8995868e6ed1ced90f87fa548575c20587b0b281
khkeipe <kkeipe@gmail.com>
2020-10-01 20:04:51 -0500

    change name to user-reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 import { loginReducer } from './login-reducer';
-import { createUserReducer } from './create-user-reducer';
+import { userReducer } from './user-reducer';
 import { quoteReducer } from './quote-reducer';
 
-export const rootReducer = combineReducers({loginReducer, createUserReducer, quoteReducer})
+export const rootReducer = combineReducers({loginReducer, userReducer, quoteReducer})
