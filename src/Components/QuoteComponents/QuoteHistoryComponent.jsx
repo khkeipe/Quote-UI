8b41afc51f621a3bb891321c97af40738cdb7aa3
khkeipe <kkeipe@gmail.com>
2020-10-01 20:06:06 -0500

    add logic to pull quotes from API

--- src/Components/QuoteComponents/QuoteHistoryComponent.jsx
+++ src/Components/QuoteComponents/QuoteHistoryComponent.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useEffect } from 'react';
-import { Segment } from 'semantic-ui-react';
+import { Card, CardContent, CardHeader, CardMeta, Grid, GridColumn, Message, Segment } from 'semantic-ui-react';
 import { getAllQuotes } from '../../remote/quote-service';
 
 const QuoteHistoryComponent = () => {
@@ -15,12 +15,19 @@ const QuoteHistoryComponent = () => {
 
 		let response = await getAllQuotes();
 			for(let quote of response) {
+				let nextQuote = {key: quote.id, order: quote.orderNumber, customer: quote.customer, date: quote.reqDate, dealer: quote.dealer};
 				quoteArray.push(
-				<div>
-					<Segment>
-						{quote}
-					</Segment>
-				</div>);
+					<Card centered fluid>
+					<CardHeader as='h2'>
+						{nextQuote.order}
+					</CardHeader>
+					<CardMeta>
+						Requested Date: {nextQuote.date}
+					</CardMeta>
+					<CardContent>
+						Dealer: {nextQuote.dealer}
+					</CardContent>
+				</Card>);
 			}
 		
 			setQuotes(quoteArray);
@@ -31,9 +38,17 @@ const QuoteHistoryComponent = () => {
 
 	return(
 		<>
-		
-		{quotes}
-
+			<Grid>
+				<GridColumn width="3">
+				</GridColumn>
+				<GridColumn width='10'>
+				{ quotes.length > 0 ? 
+					<>{quotes}</>
+					: <Segment textAlign='center'> <Message negative> No Quotes Found </Message> </Segment>}
+				</GridColumn>
+				<GridColumn width="3">
+				</GridColumn>
+			</Grid>
 		</>
 	)
 };
