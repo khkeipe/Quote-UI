b28335596baa66bf8b8f3621ecb77e06aba6392d
khkeipe <kkeipe@gmail.com>
2020-10-06 18:30:07 -0500

    remove white space

--- src/dtos/Quote.js
+++ src/dtos/Quote.js
@@ -1,7 +1,7 @@
 export class Quote {
 
 	constructor(orderNumber, reqDate, customer, dealer, pool, notes) {
-		this. orderNumber = orderNumber;
+		this.orderNumber = orderNumber;
 		this.requestDate = reqDate;
 		this.quoteCustomer = customer;
 		this.quoteDealer = dealer;
